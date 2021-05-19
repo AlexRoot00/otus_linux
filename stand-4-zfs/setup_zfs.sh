b0ee60934ea55fc660600b7f3c622e4e7ccd7c22
AlexRoot00 <65761834+AlexRoot00@users.noreply.github.com>
2021-05-19 23:44:55 +1000

    Update setup_zfs.sh

--- stand-4-zfs/setup_zfs.sh
+++ stand-4-zfs/setup_zfs.sh
@@ -19,13 +19,12 @@ zfs create testmi/userdir2 ;cd /testmi/userdir2/;curl -o "War_and_Peace.txt" -J 
 zfs create testmi/userdir3 ;cd /testmi/userdir3/;curl -o "War_and_Peace.txt" -J -L https://www.gutenberg.org/cache/epub/2600/pg2600.txt; zfs set compression=gzip testmi/userdir3;
 zfs create testmi/userdir4 ;cd /testmi/userdir4/;curl -o "War_and_Peace.txt" -J -L https://www.gutenberg.org/cache/epub/2600/pg2600.txt; zfs set compression=gzip-9 testmi/userdir4;
 zfs list -S used;
-tar -zxvf zfs_task1.tar.gz;
-cd zpoolexport; zpool import -d filea -d fileb otus;
+cd /vagrant/ ;tar -zxvf zfs_task1.tar.gz;
+cd /vagrant/zpoolexport; zpool import -d filea -d fileb otus;
 zpool status -v;
 zfs list -o recordsize; # /otus or /tank or/testmi
 zfs list -o compression;# /otus or /tank or/testmi
 zfs list -o checksum;   # /otus or /tank or/testmi
-cd .. ; zfs receive tank/otusdata < otus_task2.file
-zpool status -v;
+ cd /vagrant/; zfs receive tank/otusdata < otus_task2.file 
 cat /tank/otusdata/task1/file_mess/secret_message
 # https://github.com/sindresorhus/awesome
