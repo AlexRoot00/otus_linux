d389aacdca2a9a1c8c7ca78a58f611b36c6ce0f5
AlexRoot00 <arraccgo@gmail.com>
2021-06-06 01:39:01 +1000

    edit stands-09-systemd

--- stands-09-systemd/systemd.sh
+++ stands-09-systemd/systemd.sh
@@ -1,4 +1,7 @@
-yum install -y  epel-release;yum install -y apr spawn-fcgi httpd vim java-1.8.0-openjdk.x86_64 wget python3 psmisc;
+yum install -y  epel-release;yum install -y apr spawn-fcgi httpd vim java-1.8.0-openjdk.x86_64 wget python3 python39-setuptools;
+#python39-setuptools
+#easy_install-3.9 pip
+pip3 install flup
 # create spawn-fcgi.service
 echo '[Unit]
 Description=spawn-fcgi
@@ -10,11 +13,10 @@ ExecStop=pkill spawn-fcgi
 [Install]
 WantedBy=multi-user.target' >> /vagrant/spawn-fcgi.service
 cp /vagrant/spawn-fcgi.service /etc/systemd/system/
-#-u apache -g apache -s $SOCKET -S -M 0600 -C 32 -F 1 -P /var/run/spawn-fcgi.pid -- /usr/bin/php-cgi"
+
 #killproc spawn-fcgi
 cp /etc/init.d/spawn-fcgi /vagrant/
 rm /etc/init.d/spawn-fcgi
-
 #create dir's fcgi-servers
 mkdir /var/www/otustestnet.lan/
 cp -r /var/www/html/ /var/www/otustestnet.lan/
@@ -23,31 +25,27 @@ cp -r /var/www/html/ /var/www/otustestnet1.lan/
 #create files fcgi 
 echo "#!/usr/bin/python
 from flup.server.fcgi import WSGIServer
-from yourapplication import app
-
+from otustestnet.lan import app
 if __name__ == '__main__':
     WSGIServer(app).run()" >>/var/www/otustestnet.lan/html/app.fcgi
 chmod +x /var/www/otustestnet.lan/html/app.fcgi
+chmod 755 /var/www/otustestnet.lan/
 echo "#!/usr/bin/python
 from flup.server.fcgi import WSGIServer
-from yourapplication import app
-
+from otustestnet1.lan import app
 if __name__ == '__main__':
     WSGIServer(app).run()" >>/var/www/otustestnet1.lan/html/app.fcgi
     chmod +x /var/www/otustestnet1.lan/html/app.fcgi
+    chmod 755 /var/www/otustestnet1.lan/
 #Apache configs create
 echo '
 LoadModule fastcgi_module /usr/lib64/httpd/modules/mod_fastcgi.so
-
 FastCgiServer /var/www/otustestnet.lan/html/app.fcgi -idle-timeout 300 -processes 2
-
 <VirtualHost *>
     ServerName webapp1.mydomain.com
     DocumentRoot /var/www/otustestnet.lan/html/
-
     AddHandler fastcgi-script fcgi
     ScriptAlias / /var/www/otustestnet.lan/html/app.fcgi/
-
     <Location />
         SetHandler fastcgi-script
     </Location>
@@ -57,16 +55,12 @@ cp /vagrant/otustestnet.lan.conf /etc/httpd/conf.d/
 
 echo '
 LoadModule fastcgi_module /usr/lib64/httpd/modules/mod_fastcgi.so
-
 FastCgiServer /var/www/otustestnet.lan/html/app.fcgi -idle-timeout 300 -processes 2
-
 <VirtualHost *>
     ServerName webapp1.mydomain.com
     DocumentRoot /var/www/otustestnet1.lan/html/
-
     AddHandler fastcgi-script fcgi
     ScriptAlias / /var/www/otustestnet1.lan/html/app.fcgi/
-
     <Location />
         SetHandler fastcgi-script
     </Location>
@@ -74,6 +68,7 @@ FastCgiServer /var/www/otustestnet.lan/html/app.fcgi -idle-timeout 300 -processe
 '>> /vagrant/otustestnet1.lan.conf
 cp /vagrant/otustestnet1.lan.conf /etc/httpd/conf.d/
 #-u apache -g apache -s $SOCKET -S -M 0600 -C 32 -F 1 -P /var/run/spawn-fcgi.pid -- /usr/bin/php-cgi"
+
 #Apache.service create
 echo '[Unit]
 Description=The Apache HTTP Server
@@ -82,9 +77,7 @@ After=network.target
 Type=oneshot
 ExecStart=/usr/bin/spawn-fcgi -p 80 /sbin/httpd -f /etc/httpd/conf.d/otustestnet1.lan.conf
 ExecStart=/usr/bin/spawn-fcgi -p 443 /sbin/httpd -f /etc/httpd/conf.d/otustestnet1.lan.conf
-
 PrivateTmp=True
-
 [Install]
 WantedBy=multi-user.target' >> /vagrant/httpd.service
 cp /vagrant/httpd.service /etc/systemd/system/
@@ -100,10 +93,8 @@ sys.languageId=en">> /vagrant/response.varfile
 wget https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-software-8.17.0-x64.bin -P /vagrant/
 chmod +x /vagrant/atlassian-jira-software-8.17.0-x64.bin
 /vagrant/atlassian-jira-software-8.17.0-x64.bin -q -varfile /vagrant/response.varfile
-
 rm /vagrant/response.varfile
 rm /etc/init.d/jira
-
 #create jira.service
 echo '[Unit]
 Description=JIRA
