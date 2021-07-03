0df39373c87331bd617318b4bd08f39083b6e3f7
AlexRoot00 <arraccgoo@gmail.com>
2021-07-04 02:39:47 +1000

    add new stands

--- stands-10-ansible/ansible copy.sh
+++ stands-10-ansible/ansible copy.sh
@@ -1,2 +1,10 @@
 yum upgrade -y;
 yum install -y python3 python3-dnf ;
+sudo useradd user1 ;
+sudo useradd duser2 ;
+sudo useradd admin ;
+echo "otususer" | sudo passwd --stdin user1  ;
+echo "otususer" | sudo passwd --stdin duser2 ;
+echo "otususer" | sudo passwd --stdin admin  ;
+bash -c "sed -i
+'s/^PasswordAuthentication.*$/PasswordAuthentication yes/'/etc/ssh/sshd_config && systemctl restart sshd.service"
