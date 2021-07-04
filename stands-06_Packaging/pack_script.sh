442e71265bf8d8d0b024d8ab973f28c40d0fcea8
AlexRoot00 <arraccgoo@gmail.com>
2021-07-04 20:30:21 +1000

    update pack_script

--- stands-06_Packaging/pack_script.sh
+++ stands-06_Packaging/pack_script.sh
@@ -9,3 +9,31 @@ cp /vagrant/SPECS/nginx.spec ./rpmbuild/SPECS/
 yum-builddep ./rpmbuild/SPECS/nginx.spec -y
 rpmbuild -bb ./rpmbuild/SPECS/nginx.spec
 yum install -y rpmbuild/RPMS/x86_64/nginx-1.20.1-1.el8.ngx.x86_64.rpm 
+cat >>default.conf <<EOF
+server {
+    listen       80;
+    server_name  localhost;
+    location / {
+        root   /usr/share/nginx/html;
+        index  index.html index.htm;
+        autoindex on;
+    }
+}
+EOF
+cp default.conf /etc/nginx/conf.d/
+mkdir /usr/share/nginx/html/repo
+
+cat >> otus.repo <<EOF
+[otus]
+name=otus-linux
+baseurl=http://localhost/repo
+gpgcheck=0
+enabled=1
+EOF
+cp otus.repo /etc/yum.repos.d/
+systemctl start nginx 
+systemctl status nginx
+
+cp rpmbuild/RPMS/x86_64/nginx-1.20.1-1.el8.ngx.x86_64.rpm /usr/share/nginx/html/repo/
+createrepo /usr/share/nginx/html/repo/
+yum list | grep otus
